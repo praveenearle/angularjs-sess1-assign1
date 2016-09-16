3057f95f339fa6958601520e365c6bd9ddfbca8b
praveenearle <praveen.earle@gmail.com>
2016-09-16 12:10:47 +0530

    angulara1s1
    angulara1s1

---
+++ WebApplication5/WebApplication5/scripts/JavaScript1.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="angular.min.js" />
+
+
+var myApp = angular
+                    .module("myModule", [])
+                    .controller("myController",function ($scope) {
+
+                       
+                        $scope.firstname = "james";
+                        $scope.lastname = "bond";
+                    });
+
+ 
+
